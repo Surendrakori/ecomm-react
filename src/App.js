15ad33ae6d195c840b00d69a828aa3abb97a9d25
surendra <surendrakori@lerostech.com>
2024-03-09 13:29:01 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -2,11 +2,27 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const products = [
+  {id:1,Name : "Chai" ,Image:'./ChaiCup.jpg',price:100},
+  {id:2,Name : "Muska Bun" ,Image:"MuskaBun.jpg",price:100},
+  {id:3,Name :"biscuits" ,Image:"biscuits.jpg",price:100}
+  ]
   return (
-    <div>
-     Hello world
+    <>
+    <div>Niloufer</div>
+    <div className='container'>
+      <div className='items'>
+   { products.map((element)=> {
+        <div>
+        <div>{element.Name}</div>
+        {/* <div><img src='{element.Image}'></img></div> */}
+        <div>{element.price}</div>
+      </div>
+    })}
+    </div> 
     </div>
-  );
+    </>
+  )
 }
 
 export default App;
--- src/App1.js
+++ src/App1.js
@@ -2,6 +2,6 @@ import React from 'react'
 
 export default function App1() {
   return (
-    <div>Hi imskk</div>
+    <div>Hi skk</div>
   )
 }
