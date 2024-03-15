a0f343365a358da9302797329d8ebc3a3e65aaa7
surendra <surendrakori@lerostech.com>
2024-03-15 22:30:43 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   const products = [
-  {id:1,Name : "Chai" ,Image:'./ChaiCup.jpg',price:100},
+  {id:1,Name : "Chai" ,Image:'/ECOMM-REACT/ChaiCup.jpg',price:100},
   {id:2,Name : "Muska Bun" ,Image:"MuskaBun.jpg",price:100},
   {id:3,Name :"biscuits" ,Image:"biscuits.jpg",price:100}
   ]
@@ -15,7 +14,7 @@ function App() {
    { products.map((element)=> (
         <div>
         <div>{element.Name}</div>
-        {/* <div><img src='{element.Image}'></img></div> */}
+        <div><img src='element.Image'></img></div>
         <div>{element.price}</div>
       </div>
     ))}
