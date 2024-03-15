7d53aa7f81a86ac609e260b27791f006d2ac351b
surendra <surendrakori@lerostech.com>
2024-03-15 22:42:51 +0530

    updated

--- src/Niloufer.js
+++ src/Niloufer.js
@@ -4,7 +4,7 @@ export default function Niloufer() {
     const products = [
         {id:1,Name : "Chai" ,Image:'/ECOMM-REACT/ChaiCup.jpg',price:100},
         {id:2,Name : "Muska Bun" ,Image:"/ECOMM-REACT/MuskaBun.jpg",price:100},
-        {id:3,Name :"biscuits" ,Image:"biscuits.jpg",price:100}
+        {id:3,Name :"biscuits" ,Image:"/ECOMM-REACT/biscuits.jpg",price:100}
         ]
   return (
         <>
@@ -14,7 +14,7 @@ export default function Niloufer() {
    { products.map((element)=> (
         <div>
         <div>{element.Name}</div>
-        <div><img src='element.Image'></img></div>
+        <div><img src={element.Image}></img></div>
         <div>{element.price}</div>
       </div>
     ))}
